865d5d54940fd497a70ed08990c1f4d0c56f742f
pengt <pengtyler@gmail.com>
2025-03-12 17:50:08 -0400

    Got the news api to work now it will get info about about the market closing prices and now will get news articles that chatgpt can look at and will create an analysis for us.

--- futuresight/src/Main.py
+++ futuresight/src/Main.py
@@ -1,14 +1,18 @@
-import requests
 import pandas as pd
+import requests
+import newsapi.articles
+import newsapi.sources
 
 if __name__ == '__main__':
     # Alpha Vantage API details
     API_KEY = " VEV44TIRFGC4AVR7"
+    NEWS_API_KEY = "13da08efebbe49beba70c1cc894c066d"
     SYMBOL = "BTC"  # Replace with your stock symbol
-    DAY = 2
+    DAY = 0
     TIME = ["TIME_SERIES_DAILY", "TIME_SERIES_WEEKLY", "TIME_SERIES_MONTHLY"]
     TIME_DAY = ["Time Series (Daily)", "Weekly Time Series", "Monthly Time Series"]
     ALPHA_URL = f"https://www.alphavantage.co/query?function={TIME[DAY]}&symbol={SYMBOL}&apikey={API_KEY}&outputsize=compact"
+    NEWS_URL = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={NEWS_API_KEY}"
     LAST_X = 10
 
     # Fetch stock data
@@ -23,6 +27,13 @@ if __name__ == '__main__':
     df = df.sort_index().tail(LAST_X)  # Get the last (days,weeks,months)
 
     print(df)
+
+    # Use this to get resources to give to articles
+    # sources = newsapi.sources.Sources(API_KEY=NEWS_API_KEY)
+    # sources.get(category='business', language='en', country='us')
+
+    article = newsapi.articles.Articles(API_KEY=NEWS_API_KEY)
+    print(article.get(source="bloomberg"))
 
     # Save to Excel
     # excel_filename = "stock_prices.xlsx"
